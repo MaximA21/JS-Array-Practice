ff0c877d87f63897e3a91315a0ecc2d9459445f5
Maxim <118841284+MaximA21@users.noreply.github.com>
2023-11-27 16:22:59 +0100

    delete acc ability

--- script.js
+++ script.js
@@ -117,6 +117,17 @@ btnTransfer.addEventListener("click", function (e) {
     }
 })
 
+btnClose.addEventListener("click", function (evt) {
+    evt.preventDefault()
+
+
+    if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+        const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+        accounts.splice(index, 1)
+        containerApp.style.opacity = 0
+    }
+})
+
 
 
 
